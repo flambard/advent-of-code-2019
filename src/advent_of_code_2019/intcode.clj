fe8f87b1ff06e748f7cc87611b186ee1856f25f3
Markus Flambard <markus@flambard.se>
2019-12-05 23:22:22 +0100

    Added support for reading input and printing output

--- src/advent_of_code_2019/intcode.clj
+++ src/advent_of_code_2019/intcode.clj
@@ -2,43 +2,100 @@
   (:require [clojure.test :refer :all]))
 
 
-(defn add [{position :instruction-pointer, memory :memory}]
-  (let [term1  (memory (memory (+ 1 position)))
-        term2  (memory (memory (+ 2 position)))
-        result-position (memory (+ 3 position))]
+;;;
+;;; PARAMETER MODES
+;;;
+
+(defn- get-referenced-value [position memory]
+  (memory (memory position)))
+
+(defn- get-immediate-value [position memory]
+  (memory position))
+
+(def parameter-modes
+  {0 :position
+   1 :immediate})
+
+(defn- read-parameter [position memory parameter-mode]
+  (case parameter-mode
+    :position (get-referenced-value position memory)
+    :immediate (get-immediate-value position memory)
+    (get-referenced-value position memory)))
+
+
+;;;
+;;; INSTRUCTIONS
+;;;
+
+(defn add [{position :instruction-pointer, memory :memory} parameter-modes]
+  (let [term1 (read-parameter (+ 1 position) memory (get parameter-modes 0))
+        term2 (read-parameter (+ 2 position) memory (get parameter-modes 1))
+        result-position (get-immediate-value (+ 3 position) memory)]
     {:instruction-pointer (+ 4 position)
      :memory (assoc memory result-position (+ term1 term2))}))
 
-(defn multiply [{position :instruction-pointer, memory :memory}]
-  (let [factor1 (memory (memory (+ 1 position)))
-        factor2 (memory (memory (+ 2 position)))
-        result-position  (memory (+ 3 position))]
+(defn multiply [{position :instruction-pointer, memory :memory} parameter-modes]
+  (let [factor1 (read-parameter (+ 1 position) memory (get parameter-modes 0))
+        factor2 (read-parameter (+ 2 position) memory (get parameter-modes 1))
+        result-position (get-immediate-value (+ 3 position) memory)]
     {:instruction-pointer (+ 4 position)
      :memory (assoc memory result-position (* factor1 factor2))}))
 
-(defn stop [{memory :memory}]
+(defn store-value [{pos :instruction-pointer, memory :memory} parameter-modes]
+  (let [address (get-immediate-value (+ 1 pos) memory)
+        input (read-string (read-line))]
+    {:instruction-pointer (+ 2 pos)
+     :memory (assoc memory address input)}))
+
+(defn output-value [{pos :instruction-pointer, memory :memory} parameter-modes]
+  (let [value (read-parameter (+ 1 pos) memory (get parameter-modes 0))]
+    (println value)
+    {:instruction-pointer (+ 2 pos)
+     :memory memory}))
+
+(defn stop [{memory :memory} parameter-modes]
   {:instruction-pointer nil
    :memory memory})
 
 (def operations
   {1 add
    2 multiply
+   3 store-value
+   4 output-value
    99 stop})
 
-(defn execute-instruction [program-state]
+
+;;;
+;;; PROGRAM RUNNER
+;;;
+
+(defn- parameter-modes-vector [parameter-modes-string]
+  (mapv #(-> % (str) (read-string) (parameter-modes))
+        (reverse (str parameter-modes-string))))
+
+(defn- split-opcode-and-parameter-modes [opcode-with-parameter-modes]
+  (let [opcode (rem opcode-with-parameter-modes 100)]
+    {:opcode opcode
+     :parameter-modes (parameter-modes-vector
+                       (/ (- opcode-with-parameter-modes opcode) 100))}))
+
+(defn- execute-instruction [program-state]
   (let [{position :instruction-pointer, memory :memory} program-state
-        opcode (memory position)
+        {parameter-modes :parameter-modes
+         opcode :opcode} (split-opcode-and-parameter-modes (memory position))
         operation (operations opcode)]
     (if (nil? operation)
       (throw (ex-info "Unknown operation" program-state))
-      (let [new-state (operation program-state)]
-        (if (nil? (new-state :instruction-pointer))
-          new-state
-          (recur new-state))))))
+      (operation program-state parameter-modes))))
+
+(defn- run-until-stop-instruction [program-state]
+  (let [new-state (execute-instruction program-state)]
+    (if (nil? (new-state :instruction-pointer))
+      new-state
+      (recur new-state))))
 
 (defn run-program [program]
-  (execute-instruction {:instruction-pointer 0
-                        :memory program}))
+  (run-until-stop-instruction {:instruction-pointer 0, :memory program}))
 
 (deftest examples
   (is (= (:memory (run-program [1 9 10 3 2 3 11 0 99 30 40 50]))
